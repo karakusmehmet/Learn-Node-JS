a19095c2de8bfdf507422a7d63c71c25a06b443d
Mehmet Karakus <karakusmehmet9833@gmail.com>
2026-03-15 06:50:16 +0300

    first commit

---
+++ lesson01/server.js
@@ -0,0 +1,19 @@
+const os = require("os");
+const path = require("path");
+const { add, substract, multiply, divide } = require("./math");
+
+console.log(add(3, 4));
+console.log(multiply(3, 4));
+console.log(divide(16, 4));
+console.log(substract(16, 10));
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
+
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(path.dirname(__dirname));
+// console.log(path.basename(__dirname));
+// console.log(path.extname(__dirname));
+
+// console.log(path.parse(__filename));
